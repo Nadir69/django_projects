e01deeeb92f3e67a0839065872d3da251c7a498e
v_shabalin <v_shabalin@wargaming.net>
2026-03-19 21:57:23 +0200

    chapter08 finished

--- chapter08/myshop/myshop/settings.py
+++ chapter08/myshop/myshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
     'shop.apps.ShopConfig',
 ]
 
@@ -62,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -121,3 +125,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
--- chapter08/myshop/shop/views.py
+++ chapter08/myshop/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -8,7 +9,7 @@ def product_list(request, category_slug=None):
     products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-    products = products.filter(category=category)
+        products = products.filter(category=category)
     return render(
         request,
         'shop/product/list.html',
@@ -24,8 +25,9 @@ def product_detail(request, id, slug):
     product = get_object_or_404(
         Product, id=id, slug=slug, available=True
     )
+    cart_product_form = CartAddProductForm()
     return render(
         request,
         'shop/product/detail.html',
-        {'product': product}
+        {'product': product, 'cart_product_form': cart_product_form}
     )
